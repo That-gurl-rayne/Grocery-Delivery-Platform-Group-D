22c08c22db31001f401748cd61e22c29fc40b75c
Adesegun Martins Samad <adesegunmartins2@gmail.com>
2026-06-02 20:12:50 +0100

    updated products page.jsx

--- src/pages/Products/ProductsPage.jsx
+++ src/pages/Products/ProductsPage.jsx
@@ -11,7 +11,7 @@ import "./ProductsPage.css";
 const categories = ["All", "Drinks", "Vegetables", "Grains", "Snacks", "Fruits", "Protein"];
 
 function ProductsPage() {
-   eslint-disable-next-line
+  // eslint-disable-next-line no-unused-vars
   const [productList, setProductList] = useState(() => {
     const stored = localStorage.getItem("oya_products");
     if (stored) {
